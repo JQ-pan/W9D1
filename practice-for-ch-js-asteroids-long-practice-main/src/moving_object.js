7391244e4c8f357a1d5ac66becdeb7315a638343
Wilson Wu <wuyecheng738@gmail.com>
2023-01-09 15:10:19 -0500

    draw circle

---
+++ practice-for-ch-js-asteroids-long-practice-main/src/moving_object.js
@@ -0,0 +1,15 @@
+console.log("MO is working")
+
+function MovingObject(object) {
+  this.pos = object.pos;
+  this.vel = object.vel;
+  this.radius = object.radius;
+  this.color = object.color;
+
+}
+
+MovingObject.prototype.draw = function(ctx) {
+  
+}
+
+module.exports = MovingObject
